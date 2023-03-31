e61ecd2ad2beb2141ee3e29f4913e5c7dc5bff18
johnie261 <njorogejohn261@gmail.com>
2023-03-31 05:21:26 +0300

    Update app.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Books from './components/books';
+import Category from './components/Category';
+import BookNav from './components/BookNav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BookNav />}>
+          <Route index element={<Books />} />
+          <Route path="category" element={<Category />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
