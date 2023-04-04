4336c01c9fa5d2317c312e1097c01c2174c770dc
johnie261 <njorogejohn261@gmail.com>
2023-04-04 13:47:07 +0300

    Make requested changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './book/bookSlice';
 import categoryReducer from './category/categorySlice';
 
-/* eslint-disable import/prefer-default-export */
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     book: bookReducer,
     category: categoryReducer,
   },
 });
+
+export default store;
